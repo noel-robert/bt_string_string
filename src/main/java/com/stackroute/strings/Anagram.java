6be5ac04954fe39d409738520292a4409489ff70
Noel John Robert <noel060202@gmail.com>
2024-03-17 16:26:46 +0530

    all test cases passed

--- src/main/java/com/stackroute/strings/Anagram.java
+++ src/main/java/com/stackroute/strings/Anagram.java
@@ -1,8 +1,29 @@
 package com.stackroute.strings;
 
+import java.util.Arrays;
+
 public class Anagram {
     //write logic to check given two phrases are anagrams or not and return result
     public String checkAnagrams(String phraseOne, String phraseTwo) {
-        return null;
+        // if any one is empty, return appropriate message
+        if (phraseOne.isEmpty() || phraseTwo.isEmpty()) return "Give proper input not empty phrases";
+        
+        // if length of both are different, they cannot be Anagram
+        if (phraseOne.length() != phraseTwo.length()) return "Given phrases are not anagrams";
+        
+        // convert strings to character arrays and then sort
+        char[] phraseOneArray = phraseOne.toCharArray();
+        Arrays.sort(phraseOneArray);
+        
+        char[] phraseTwoArray = phraseTwo.toCharArray();
+        Arrays.sort(phraseTwoArray);
+
+        // create new String objects 
+        String phraseOneSorted = new String(phraseOneArray);
+        String phraseTwoSorted = new String(phraseTwoArray);
+
+        // compare and return appropriate error message
+        if (phraseOneSorted.equals(phraseTwoSorted)) return "Given phrases are anagrams";
+        return "Given phrases are not anagrams";
     }
 }
